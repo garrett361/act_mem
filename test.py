c9871ee9535b9f28cb135a70369ed4229a036074
Garrett Goon <garrett.goon@hpe.com>
2024-05-30 21:59:24 -0400

    cuda tests

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import pytest
 import torch
 import torch.nn as nn
@@ -9,6 +11,8 @@ DIM = 128
 SEQ_LEN = 64
 N_HEADS = 4
 BATCH_SIZE = 2
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 ZERO_MEM_ACT_FNS = [nn.ReLU(), nn.Sigmoid(), nn.Tanh(), nn.LeakyReLU(inplace=True)]
 ALL_ACT_FNS = ZERO_MEM_ACT_FNS + [
@@ -24,14 +28,14 @@ ALL_ACT_FNS = ZERO_MEM_ACT_FNS + [
 ]
 
 
-class TestSavedBwdCaptureContext:
+class TestSavedTensorContext:
     def test_linear(self) -> None:
         """
         Test simple linear layer. The inputs should be saved for backwards
         """
-        inputs = torch.randn(1, DIM, requires_grad=True)
-        lin = nn.Linear(DIM, DIM)
-        with act_mem.SavedBwdCaptureContext(ignored_tensors=lin.parameters()) as saved:
+        inputs = torch.randn(1, DIM, requires_grad=True, device=DEVICE)
+        lin = nn.Linear(DIM, DIM, device=DEVICE)
+        with act_mem.SavedTensorContext(ignored_tensors=lin.parameters()) as saved:
             _ = lin(inputs)
         assert saved.saved_tensor_mem == inputs.numel() * inputs.element_size()
 
@@ -40,11 +44,11 @@ class TestSavedBwdCaptureContext:
         Test a linear layer with AMP. The saved tensors should now be a low-precision version of the
         inputs and the low-precision version of the weights version of the weights
         """
-        inputs = torch.randn(1, DIM, requires_grad=True)
-        lin = nn.Linear(DIM, DIM)
+        inputs = torch.randn(1, DIM, requires_grad=True, device=DEVICE)
+        lin = nn.Linear(DIM, DIM, device=DEVICE)
         dtype = torch.bfloat16
-        with torch.autocast(device_type="cpu", dtype=dtype):
-            with act_mem.SavedBwdCaptureContext(ignored_tensors=lin.parameters()) as saved:
+        with torch.autocast(device_type=DEVICE, dtype=dtype):
+            with act_mem.SavedTensorContext(ignored_tensors=lin.parameters()) as saved:
                 out = lin(inputs)
         assert (
             saved.saved_tensor_mem
@@ -52,7 +56,8 @@ class TestSavedBwdCaptureContext:
         )
 
     @pytest.mark.parametrize("act_fn", ALL_ACT_FNS)
-    def test_mlp(self, act_fn: nn.Module) -> None:
+    @pytest.mark.parametrize("dropout_prob", (None, 0.5))
+    def test_mlp(self, act_fn: nn.Module, dropout_prob: Optional[float]) -> None:
         """
         For the transformer MLP layer with a ReLU non-linearity, the initial inputs and the inputs
         to the final linear layer (which are four times as large) must always be saved. If the
@@ -61,33 +66,37 @@ class TestSavedBwdCaptureContext:
         times as large as the MLP's inputs). The MLP activation memory can be nearly halved by a
         choice of activation function.
         """
-        inputs = torch.randn(1, DIM, requires_grad=True)
+        inputs = torch.randn(1, DIM, requires_grad=True, device=DEVICE)
         expansion_factor = 4
-        mlp = layers.MLP(d_model=DIM, act_fn=act_fn)
-        with act_mem.SavedBwdCaptureContext(ignored_tensors=mlp.parameters()) as saved:
+        mlp = layers.MLP(d_model=DIM, act_fn=act_fn, dropout_prob=dropout_prob, device=DEVICE)
+        with act_mem.SavedTensorContext(ignored_tensors=mlp.parameters()) as saved:
             _ = mlp(inputs)
 
         # Compare measured memory against expected
-        first_lin_input_mem = inputs.numel() * inputs.element_size()
-        second_lin_input_mem = expansion_factor * inputs.numel() * inputs.element_size()
+        first_lin_input_mem = act_mem.get_tensor_bytes(inputs)
+        second_lin_input_mem = expansion_factor * first_lin_input_mem
         # Only some activations require additional activation memory
         activation_input_mem = 0 if act_fn in ZERO_MEM_ACT_FNS else second_lin_input_mem
+        dropout_act_mem = 0 if not dropout_prob else inputs.numel() * (4 if DEVICE == "cpu" else 1)
 
-        expected_mem = first_lin_input_mem + second_lin_input_mem + activation_input_mem
-        assert saved.saved_tensor_mem == expected_mem, f"Failed on {act_fn=}"
+        expected_mem = (
+            first_lin_input_mem + second_lin_input_mem + activation_input_mem + dropout_act_mem
+        )
+        assert saved.saved_tensor_mem == expected_mem, f"Failed on {act_fn=}, {dropout_prob=}"
 
     @pytest.mark.parametrize("act_fn", ALL_ACT_FNS)
-    def test_mlp_amp(self, act_fn: nn.Module) -> None:
+    @pytest.mark.parametrize("dropout_prob", (None, 0.5))
+    def test_mlp_amp(self, act_fn: nn.Module, dropout_prob: Optional[float]) -> None:
         """
         Similar story with AMP. The only changes come from the modified dtypes and needing to also
         save references to the low-precision weights in the Linear layers.
         """
-        inputs = torch.randn(1, DIM, requires_grad=True)
+        inputs = torch.randn(1, DIM, requires_grad=True, device=DEVICE)
         expansion_factor = 4
-        mlp = layers.MLP(d_model=DIM, act_fn=act_fn)
+        mlp = layers.MLP(d_model=DIM, act_fn=act_fn, dropout_prob=dropout_prob, device=DEVICE)
         dtype = torch.bfloat16
-        with torch.autocast(device_type="cpu", dtype=dtype):
-            with act_mem.SavedBwdCaptureContext(ignored_tensors=mlp.parameters()) as saved:
+        with torch.autocast(device_type=DEVICE, dtype=dtype):
+            with act_mem.SavedTensorContext(ignored_tensors=mlp.parameters()) as saved:
                 _ = mlp(inputs)
 
         # Compare measured memory against expected
@@ -96,11 +105,18 @@ class TestSavedBwdCaptureContext:
         second_lin_input_mem = expansion_factor * inputs.numel() * dtype.itemsize
         # Only some activations require additional activation memory
         activation_input_mem = 0 if act_fn in ZERO_MEM_ACT_FNS else second_lin_input_mem
+        dropout_act_mem = (
+            0 if not dropout_prob else inputs.numel() * (dtype.itemsize if DEVICE == "cpu" else 1)
+        )
 
         expected_mem = (
-            amp_weight_mem + first_lin_input_mem + second_lin_input_mem + activation_input_mem
+            amp_weight_mem
+            + first_lin_input_mem
+            + second_lin_input_mem
+            + activation_input_mem
+            + dropout_act_mem
         )
-        assert saved.saved_tensor_mem == expected_mem, f"Failed on {act_fn=}"
+        assert saved.saved_tensor_mem == expected_mem, f"Failed on {act_fn=}, {dropout_prob=}"
 
 
 @pytest.mark.skipif(not torch.cuda.is_available(), reason="cuda not available")
@@ -111,19 +127,19 @@ class TestCUDAMemReadings:
 
 class TestLayers:
     def test_mlp(self) -> None:
-        mlp = layers.MLP(d_model=DIM, act_fn=nn.ReLU())
-        inputs = torch.randn(BATCH_SIZE, SEQ_LEN, DIM)
+        mlp = layers.MLP(d_model=DIM, act_fn=nn.ReLU(), device=DEVICE)
+        inputs = torch.randn(BATCH_SIZE, SEQ_LEN, DIM, device=DEVICE)
         outputs = mlp(inputs)
         assert outputs.shape == inputs.shape
 
     def test_attention(self) -> None:
-        attn = layers.Attention(d_model=DIM, n_heads=N_HEADS)
-        inputs = torch.randn(BATCH_SIZE, SEQ_LEN, DIM)
+        attn = layers.Attention(d_model=DIM, n_heads=N_HEADS, device=DEVICE)
+        inputs = torch.randn(BATCH_SIZE, SEQ_LEN, DIM, device=DEVICE)
         outputs = attn(inputs)
         assert outputs.shape == inputs.shape
 
     def test_block(self) -> None:
-        block = layers.Block(d_model=DIM, n_heads=N_HEADS, act_fn=nn.ReLU())
-        inputs = torch.randn(BATCH_SIZE, SEQ_LEN, DIM)
+        block = layers.Block(d_model=DIM, n_heads=N_HEADS, act_fn=nn.ReLU(), device=DEVICE)
+        inputs = torch.randn(BATCH_SIZE, SEQ_LEN, DIM, device=DEVICE)
         outputs = block(inputs)
         assert outputs.shape == inputs.shape
